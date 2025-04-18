3a8e49b8edf417a7b2f2548ca03f602b0688ba2b
Max Sikorsky <pain.maker28@gmail.com>
2025-04-18 09:54:07 +0100

    first commit

---
+++ src/main/java/ru/mentee/power/variables/Main.java
@@ -0,0 +1,16 @@
+package ru.mentee.power;
+
+
+import static java.lang.System.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int age = 35;
+        String name = "Максим";
+
+        out.println("имя : " + name);
+        out.println("возраст: " + age);
+        
+    }
+
+}
